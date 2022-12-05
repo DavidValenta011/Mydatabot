31ab519eede3d44315f1616679ad7b50c0b1efc8
DavidV <valentd1@gitlab.mff.cuni.cz>
2022-12-05 23:33:02 +0100

    folder cleanup

---
+++ implementation/resources/containers.py
@@ -0,0 +1,3 @@
+class Resource():
+    def __init__(self, iri):
+        self.iri = iri
--- implementation/resources/resources.py
+++ implementation/resources/resources.py
@@ -1,5 +1,8 @@
 print("hura")
 
+import numpy as np
+import pandas as pd
+
 class Resource():
     def __init__(self, iri):
         self.iri = iri
@@ -13,3 +16,6 @@ class NonRdfResource(Resource):
     def __init__(self, iri, data):
         super().__init__(iri)
         self.data = data
+
+if __name__ == "__main__":
+    testobject = NonRdfResource("iririr", pd.DataFrame())
